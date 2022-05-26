fd65ca9ba3b2c2ec2b95580c48de4678df3e84b1
Loïc Ruffiot <loicruffiot@gmail.com>
2022-05-26 16:55:24 +0200

    first commmit

---
+++ __main__.py
@@ -0,0 +1,39 @@
+from main_request import main, catch_images
+
+URL = "http://books.toscrape.com/"
+
+
+def display():
+
+    print("""
+            ----------------------------------------
+            Bienvenu dans l'outil d'extraction de donnée
+
+            choisissez une option :
+
+            1 : Extraire les informations des livres
+            2 : Extraire les couvertures des livres
+            3 : Quitter l'application
+            ----------------------------------------
+            """
+            )
+            
+    choix = input("Votre choix -> ")
+    choix_menu = ["1","2","3"]
+
+    while choix in choix_menu:
+
+        if choix  == "1":
+            main(URL)
+
+        elif choix == "2":
+            catch_images(URL)
+
+        elif choix == "3":
+            print("Au revoir")
+            exit()
+
+    print('Saisi invalide')
+    return display()
+
+display()
